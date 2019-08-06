92812dfcd7f1a9acaffcf7a30d49bce023b14540
Metin137 <mtnklc137@gmail.com>
2019-08-06 18:32:47 +0300

    ilkk

---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import bgMessaging from './bgMessaging'; // <-- Import the file you created in (2)
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessaging); // <-- Add this line
